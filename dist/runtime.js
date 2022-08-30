0a881604e3969251505aebbef106e61b1ef30bfc
Saeqa Sultani <saeqasultani@gmail.com>
2022-08-30 17:35:22 +0430

    deploy To-Do-List-Project the gh-pages

--- dist/runtime.js
+++ dist/runtime.js
@@ -190,7 +190,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkwebpack"] = self["webpackChunkwebpack"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkto_do_list_project"] = self["webpackChunkto_do_list_project"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
