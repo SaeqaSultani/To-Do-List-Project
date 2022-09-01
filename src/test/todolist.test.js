ea86f8fbd9d4ebc1722230987dd22bb5660aa5b9
unknown <okonjogilbert2@gmail.com>
2022-09-01 14:40:17 +0300

    modify add and remove method

--- src/test/todolist.test.js
+++ src/test/todolist.test.js
@@ -5,7 +5,7 @@ import getdata from '../module/getData.js';
 
 describe('Test methods', () => {
   test('should add data to the list', () => {
-    addData('hi');
+    addData('the');
 
     const data = getdata();
 
@@ -13,7 +13,7 @@ describe('Test methods', () => {
   });
 
   test('should delete data from the list', () => {
-    deletTasksFromLocalSorage('hi');
+    deletTasksFromLocalSorage('the');
     const arr = getdata();
 
     expect(arr.length).toBe(0);
